82de724262d8a835c92db25e83068a7c13c68839
BakaryGibba <162592884+BakaryGibba@users.noreply.github.com>
2024-03-12 23:47:00 +0800

    Add files via upload

---
+++ Bakary.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Bakary {
+    public static void main(String[] args){
+        Scanner marks= new Scanner(System.in);
+        System.out.println("Input your mark: ");
+        float numb1= marks.nextFloat();
+        System.out.println("You scored " +numb1);
+
+    }
+}
