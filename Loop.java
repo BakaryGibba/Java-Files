9664fb95cd7b84a6536c348c1adad285ed858816
BakaryGibba <162592884+BakaryGibba@users.noreply.github.com>
2024-03-12 23:48:39 +0800

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class Loop {
+    public static void main(String[] args) {
+       /* int num;
+        for (num=1; num <= 5; num+= 1){
+            System.out.println(num);*/
+
+
+        for (int num=1; num <= 5; num+= 1)
+        {System.out.println("Helo World");
+        }
+    }
+}
